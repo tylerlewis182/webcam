bb355cae669f805428098ae6b04562a70e874150
Tyler Lewis <tylerlewis182@gmail.com>
2019-04-02 21:25:52 -0500

    Added launch files

--- src/webcam_subscriber.cpp
+++ src/webcam_subscriber.cpp
@@ -18,12 +18,12 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "image_listener");
+  ros::init(argc, argv, "webcam_subscriber");
   ros::NodeHandle nh;
   cv::namedWindow("view");
   cv::startWindowThread();
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
+  image_transport::Subscriber sub = it.subscribe("webcam/image", 1, imageCallback);
   ros::spin();
   cv::destroyWindow("view");
 }
